20ac26fe16255e94ea97b6778a17b6457efa8ec4
Mateo Quintero C <85648547+Mateoquinteroc@users.noreply.github.com>
2024-02-19 12:15:03 -0500

    12:15

--- src/PrincipalCard.jsx
+++ src/PrincipalCard.jsx
@@ -9,7 +9,7 @@ const PrincipalCard = () => {
            <h2 className=" ml-7 4xl:text-6xl text-xl text-rojo font-light bg-white pl-2 w-fit font-open-sans pr-3 mix-blend-lighten">Centro Cultural</h2>
            <p className=" absolute top-5 left-1/2 transform -translate-x-1/2   4xl:text-6xl text-xl text-white font-bold font-open-sans text-center align-top mix-blend-lighten 3xl:text-4xl">Conoce la programación <br/> que tenemos para ti</p>
            <div className="absolute top-0 right-0 w-auto h-auto mt-10 font-open-sans 3xl:mt-2">
-            <p className=" 4xl:text-4xl text-xl text-white font-black  font-open-sans  m-5 pr-2 3xl:m-1 3xl:w-fit">Del 19 al 23</p> 
+            <p className=" 4xl:text-5xl text-xl text-white font-black  font-open-sans  m-5 pr-2 3xl:m-1 3xl:w-fit">Del 19 al 23</p> 
             <p className=" m-5 pr-2 4xl:text-5xl text-xl text-white font-normal font-open-sans 3xl:m-0 4xl:absolute 4xl:right-5">de febrero </p> 
             <p className="pl-[50%] m-5 4xl:text-5xl text-xl text-white font-normal font-open-sans 3xl:m-0 4xl:mt-[4vh] 4xl:pl-[60%] ">2024</p> 
            </div>
