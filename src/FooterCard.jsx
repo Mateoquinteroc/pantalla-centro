2d7262b6efcf34880f3e79b755be00f3e3f73ed9
Mateo Quintero C <85648547+Mateoquinteroc@users.noreply.github.com>
2024-02-26 15:27:08 -0500

    cambio xl3

--- src/FooterCard.jsx
+++ src/FooterCard.jsx
@@ -14,8 +14,8 @@ const FooterCard = () => {
     return (
         <div className="absolute bottom-0  w-full gap-x-[500px] ">
             {/* <button className={`absolute  h-fit w-fit 4xl:h-[100px] 4xl:w-[300px] bottom-[3vh] left-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full z-10 font-open-sans`} onClick={() => {Context.openCardBaseDetails()}}>Recorrido</button> */}
-            <button className="absolute m-0 w-fit h-fit 4xl:h-[100px] 4xl:w-[400px] left-10 4xl:bottom-[2vh] bg-blue-500 hover:bg-blue-700 text-white text-4xl font-bold py-2 px-4 rounded-full z-10 font-open-sans " onClick={() => {Context.openQr()}}>ChatBOT</button>
-            <figure className="absolute   4xl:mt-0 r 3xl:w-[30%] 3xl:bottom-0 4xl:right-10 ">
+            <button className="absolute m-0 w-fit h-fit 4xl:h-[100px] 3xl:h-[80px] 4xl:w-[400px] left-10 4xl:bottom-[2vh] 3xl:bottom-4 bg-blue-500 hover:bg-blue-700 text-white text-4xl font-bold py-2 px-4 rounded-full z-10 font-open-sans " onClick={() => {Context.openQr()}}>ChatBOT</button>
+            <figure className="absolute   4xl:mt-0 r 3xl:w-[30%] 3xl:bottom-0 3xl:right-10 4xl:right-10 ">
             <img className="bottom-0 mt-[5vh] 3xl:mt-0  4xl:mt-0 z-index-10 " src={logos}/>
 
            </figure>
